8de6ea278f0e0a5f60d7a88bb49cdcb933b6d878
charlotteLake <celake@gmail.com>
2017-11-14 19:19:35 -0500

    added event handler for click on card

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 
-document.getElementsByClassName("card").style.backgroundColor = "#0000ff";
+// document.getElementsByClassName("card").style.backgroundColor = "#0000ff";
+
+//$( "#deck" ).click(function() {
+//  console.log( "Handler for .click() called." );
+//});
 
 
+$(".card").click(function() {
+	console.log( "Handler for .click() called." );
+	$(this).css("backgroundColor", "#0000ff");
+})
 
 
 //icon[0].on("click", function() {
