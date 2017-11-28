4772400aca955d5d2be20e515eaab2932562e0a3
charlotteLake <celake@gmail.com>
2017-11-28 17:24:23 -0500

    basic card flip added

--- js/app.js
+++ js/app.js
@@ -1,17 +1,10 @@
 
-// document.getElementsByClassName("card").style.backgroundColor = "#0000ff";
-
-//$( "#deck" ).click(function() {
-//  console.log( "Handler for .click() called." );
-//});
-
-
-$(".card").click(function() {
-	console.log( "Handler for .click() called." );
-	$(this).css("backgroundColor", "#0000ff");
-})
+$('.card').click(
+	function () {
+		console.log("I heard the click")
+		$(this).addClass('theFlip');
+});
 
 
-//icon[0].on("click", function() {
-//	console.log("clicked on the circle");
-//});
+
+
