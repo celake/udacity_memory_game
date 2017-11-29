5f859c307f1db96ad95d6adc9d5bd64e326ad8e7
charlotteLake <celake@gmail.com>
2017-11-28 19:17:22 -0500

    conditional logic to count unmatched cards

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
+var numCards = $(".noMatch");
 
-$('.card').click(
+if (numCards.length > 0) {
+	$('.card').click(
 	function () {
-		console.log("I heard the click")
+		console.log("I heard the click");
 		$(this).addClass('theFlip');
-});
+		$(this).addClass('match');
+		$(this).removeClass('noMatch');
+	});
+	} else if (numCards == 0) {	
+		console.log("You have made your last click")
+}
+
+	
+
 
 
 
