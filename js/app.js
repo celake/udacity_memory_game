9f8320b59db03ecef4bc19ed11237907217a6db3
charlotteLake <celake@gmail.com>
2017-11-28 21:09:08 -0500

    Added conditonal to check for matching cards.  Bug: does not execute if statement line 12.

--- js/app.js
+++ js/app.js
@@ -9,12 +9,25 @@ if (numCards.length > 0) {
 		$(this).addClass('flipped');
 		$(this).removeClass('noMatch');
 	});
+			if (numFlipped.length == 2){
+				var icon = document.getElementsByClassName('flipped fa')
+				console.log("numFlipped is 2")
+					if (icon[0] === icon[1]) {
+						console.log("yay its a match");
+						$(this).addClass('match');
+						$(this).removeClass('noMatch');
+					} else {
+						console.log("it's not a match");
+						$(this).removeClass('theFlip');
+		
+					}
+			}
 
-	} else if (numCards.length == 0) {	
+} else if (numCards.length == 0) {	
 
-		// you win animation goes here //
+	// you win animation goes here //
 
-		console.log("your done!!");
+	console.log("your done!!");
 }
 
 	
